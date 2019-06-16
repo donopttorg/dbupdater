1e4c566f582cf38449383767f17d8396e22fe4d7
NekitMalyarenko <n.a.m.62608@gmail.com>
2019-06-16 13:45:32 +0300

    fix#2

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"github.com/NekitMalyarenko/donopttorg-dbupdater/updater"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"log"
+)
+
+const envConfigFile = "env.json"
+
 func main() {
-	StartUpdater()
+	setSystemEnv()
+	updater.StartUpdater()
+}
+
+func setSystemEnv() {
+	if _, err := os.Stat(envConfigFile); !os.IsNotExist(err) {
+		file, _  := os.Open(envConfigFile)
+		raw, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data := map[string]string{}
+		err = json.Unmarshal(raw, &data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for k, v := range data {
+			os.Setenv(k, v)
+		}
+	}
 }
--- updater/getter.go
+++ updater/getter.go
@@ -1,4 +1,4 @@
-package main
+package updater
 
 import (
 	"github.com/juju/errors"
